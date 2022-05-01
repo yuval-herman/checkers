428fc58190609f084c5ceca3a26d05fb2536cbfc
yuval <nhajeho1@gmail.com>
2022-05-01 15:01:04 +0300

    implement getMoves for Piece

--- Piece.js
+++ Piece.js
@@ -3,4 +3,22 @@ class Piece {
 		this.color = color; // Color is boolean, dark is false white is true.
 		this.imgPath = imgPath;
 	}
+
+	getMoves(pos, boardArr) {
+		return this.filterMoves(
+			this.color
+				? [pos.add(new Vector(1, 1)), pos.add(new Vector(1, -1))]
+				: [pos.add(new Vector(-1, -1)), pos.add(new Vector(-1, 1))]
+		);
+	}
+
+	filterMoves(moves, boardArr) {
+		return moves.filter((e) => !this.isOutOfBounds(e));
+	}
+
+	isOutOfBounds(pos) {
+		return (
+			pos.x >= BOARD_SIZE || pos.y >= BOARD_SIZE || pos.x < 0 || pos.y < 0
+		);
+	}
 }
